aa63759fb0664365851679959ab117483d634f37
Miguel <pmiguel2008@hotmail.es>
2025-04-30 18:59:33 +0200

    Corrijo ruta de base de datos y confirmo creación de tablas Producto, Proveedor y Venta

--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))   # Obtiene la ruta absoluta del directorio actual
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '/instance/tienda.db')  # Cambia esto por la URI de mi base de datos
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.dirname(basedir), 'instance', 'tienda.db') # Cambia esto por la URI de mi base de datos
 SQLALCHEMY_TRACK_MODIFICATIONS = False  # Desactiva la advertencia de modificaciones de objetos
 SECRET_KEY = os.urandom(24)   # Cambia esto por una clave secreta más segura en producción, Sirve para la seguridad de sesiones/cookies en Flask.
